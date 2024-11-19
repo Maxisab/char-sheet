1ceafc1d8c25ee346251dbe1e0f48ee00f822b86
maxisab <max.sabate@gmail.com>
2024-11-19 15:18:02 -0800

    working on class cards, still unsure how to implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Cards } from './Cards/Cards'
 import { Nav } from './Navbar/Nav'
 
 function App() {
 
   return (
     <>
-      <Nav/>
+      <Nav />
+      <Cards />
     </>
   )
 }
---
+++ src/Cards/Cards.tsx
@@ -0,0 +1,18 @@
+export const Cards = () => {
+  return (
+    <div className="text-blue-500 *:h-20 flex-col *:flex *:justify-center *:items-center">
+    <div className="alchemist bg-amber-400">Alchemist</div>
+    <div className="barbarian bg-red-400">Barbarian</div>
+    <div className="bard">Bard</div>
+    <div className="champion">Champion</div>
+    <div className="cleric">Cleric</div>
+    <div className="druid">Druid</div>
+    <div className="Fighter">Fighter</div>
+    <div className="Monk">Monk</div>
+    <div className="Ranger">Ranger</div>
+    <div className="Rogue">Rogue</div>
+    <div className="Sorcerer">Sorcerer</div>
+    <div className="Wizard">Wizard</div>
+    </div>
+  )
+}
