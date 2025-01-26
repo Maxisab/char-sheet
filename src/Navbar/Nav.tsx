7359ae933db8fda06bb31b8588cb8a7d964031db
Max Sabate <max.sabate@gmail.com>
2025-01-25 16:56:38 -0800

    adding ancestry page, still working out issues

--- src/Navbar/Nav.tsx
+++ src/Navbar/Nav.tsx
@@ -2,11 +2,17 @@ import { useLocation } from "react-router-dom";
 
 interface NavProps {
   filterClasses: boolean;
+  filterAncestries: boolean;
   setFilterClasses: (value: boolean) => void;
+  setFilterAncestries: (value: boolean) => void;
 }
 
-export const Nav = ({ filterClasses, setFilterClasses }: NavProps) => {
-  const handleToggle = () => setFilterClasses(!filterClasses);
+export const Nav = ({ 
+  filterClasses, 
+  filterAncestries, 
+  setFilterClasses, 
+  setFilterAncestries 
+}: NavProps) => {
   const location = useLocation()
 
   return (
@@ -23,7 +29,24 @@ export const Nav = ({ filterClasses, setFilterClasses }: NavProps) => {
             <input
               type="checkbox"
               checked={filterClasses}
-              onChange={handleToggle}
+              onChange={() => setFilterClasses(!filterClasses)}
+              className="sr-only peer"
+            />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:bg-gray-700 peer-checked:bg-blue-600 rounded-full peer-checked:after:translate-x-6 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+          </label>
+        </div>
+        </div>
+      )}
+      {location.pathname === '/character-creation/ancestry' && (
+        <div className="flex items-center justify-between bg-gray-800 p-4 text-primary-parchment fixed top-0 w-full z-50">
+        <h1 className="text-lg font-bold">Select a class</h1>
+        <div className="flex items-center gap-4">
+          <span>Filter Remaster</span>
+          <label className="relative inline-flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              checked={filterAncestries}
+              onChange={() => setFilterAncestries(!filterAncestries)}
               className="sr-only peer"
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:bg-gray-700 peer-checked:bg-blue-600 rounded-full peer-checked:after:translate-x-6 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
